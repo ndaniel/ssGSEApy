f8a66a154dd22638b2014cd688ee21fcdd0fd838
Daniel Nicorici <Daniel.Nicorici@gmail.com>
2023-03-06 14:07:05 +0200

    Update ssgsea.py

--- ssgsea.py
+++ ssgsea.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
-# Author: Daniel.Nicorici@gmail.com
-# MArch 6, 2023
+# -*- coding: utf-8 -*-
+"""
+It implements the ssGSEA method (Subramanian et al. PNAS 2005).
 
-# implements the ssGSEA method (Subramanian et al. PNAS 2005)
+Author: Daniel Nicorici, Daniel.Nicorici@gmail.com
+Copyright (c) 2023 Daniel Nicorici
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 
 
-def ssgsea(x, x_rows_genes, geneset, alpha = 0.25, scale = True, norm = False, min_genes=15,max_genes=500):
+
+
+def ssgsea(x, x_rows_genes, geneset, alpha = 0.25, scale = True, norm = False, min_genes=15, max_genes=500):
     """
     x = matrix as list of lists, ie. list of rows (rows = genes, columns = samples)
-    x_rowsgenes = labels for rows of x that are the gene names (eg. genes)
+    x_rows_genes = labels for rows of x that are the gene names (eg. genes)
     geneset = one set of genes
     """
-    genes =set(geneset).intersection(x_rows_genes)
+    genes = set(geneset).intersection(x_rows_genes)
 
     n = len(genes)
     if n < min_genes or n > max_genes or (not x):
